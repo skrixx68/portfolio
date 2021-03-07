3bb9098c00339ededf7ef6fa62c9fb3b29774b57
Skrixx68 <35738176+skrixx68@users.noreply.github.com>
2021-03-07 19:30:03 +0800

    visit counter

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once("index.html"); ?>
+<?php include_once("index.html"); 
+$myfile = fopen("visits.txt", "r") or die("Unable to open file!");
+$count = intval(fgets($myfile)) + 1;
+console.log( $count);
+fclose($myfile);
+$myfile = fopen("visits.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $count);
+fclose($myfile);
+?>
