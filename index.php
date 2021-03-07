961ef9bc10c8bcad556f39489758531de8eff2bf
Skrixx68 <35738176+skrixx68@users.noreply.github.com>
2021-03-07 19:39:11 +0800

    remove counter

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
-<?php include_once("index.html"); 
-$myfile = fopen("visits.txt", "r") or die("Unable to open file!");
-$count = intval(fgets($myfile)) + 1;
-console.log( $count);
-fclose($myfile);
-$myfile = fopen("visits.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $count);
-fclose($myfile);
+<?php include_once("index.html");
 ?>
